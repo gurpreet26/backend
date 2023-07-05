62203ff2cd362c05f8935164281d6524beb50bb6
Gurpreet singh <33153390+gurpreet26@users.noreply.github.com>
2023-07-05 10:48:45 -0400

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('')
+        await mongoose.connect('db url')
         console.log("mongodb connected ")
         
     } catch (error) {
